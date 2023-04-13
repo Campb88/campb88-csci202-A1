838eed0fe04dfed7cddd8d9040b62a7840f7cc80
Campb88 <108759113+Campb88@users.noreply.github.com>
2023-04-13 07:04:33 -0700

    Update script.js

--- A3/script.js
+++ A3/script.js
@@ -1,48 +1,34 @@
 function animated(x) {
 
   var body = document.getElementById("body");
-  body.style.animation = "body 30s infinite";
+  body.style.animation = "body 20s infinite";
 
   var i = document.getElementById("next");
-  i.style.animation = "icon 30s infinite";
+  i.style.animation = "icon 20s infinite";
 
   var container = document.getElementById("container");
-  container.style.animation = "container 30s infinite";
+  container.style.animation = "container 20s infinite";
 
   var t2 = document.getElementById("t2");
-  t2.style.animation = "t2 30s infinite";
+  t2.style.animation = "t2 20s infinite";
 
 
   var t1 = document.getElementById("t1");
-  t1.style.animation = "t1 30s infinite";
+  t1.style.animation = "t1 20s infinite";
 
 
   var t3 = document.getElementById("t3");
-  t3.style.animation = "t3 30s infinite";
+  t3.style.animation = "t3 20s infinite";
 
 
   var t4 = document.getElementById("t4");
-  t4.style.animation = "t4 30s infinite";
+  t4.style.animation = "t4 20s infinite";
 
 
   var t5 = document.getElementById("t5");
-  t5.style.animation = "t5 30s infinite";
+  t5.style.animation = "t5 20s infinite";
 
 
   var t6 = document.getElementById("t6");
-  t6.style.animation = "t6 30s infinite";
-}
-
-function goodbye(x) {
-
-  var i = document.getElementById("next");
-
-  var title = document.getElementById("title");
-  title.style.transition = "ease-out 3s";
-  title.style.opacity = 0;
-
-  var container = document.getElementById("container");
-  container.style.transition = "ease-out 3s";
-  container.style.opacity = 0;
-
+  t6.style.animation = "t6 20s infinite";
 }
